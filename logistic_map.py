4cee189376763f803871ec6bab9891be54cb32b8
JuneKim0712 <Junwoo.Kim0712@gmail.com>
2020-11-23 22:18:03 -0800

    fixed many many bugs and improved readability

--- logistic_map.py
+++ logistic_map.py
@@ -12,24 +12,24 @@ def simplify(list_):
             return b
  
  
-class GraphIt:
-    def __init__(self, initial_population, growth_rate):
-        self.gr_rate = np.float(growth_rate)
-        self.initial = initial_population
+class LogisticMap:
+    def __init__(self):
+        self.growth_rate = np.float(0)
+        self.initial = 0.5
         self.all_result = np.array([], dtype=float)
-        self.test_popul = initial_population
+        self.test_initial = self.initial
         self.limit = 4.0
     
     def plot(self, finding, times):
         times += 100
         while times > 1:
             self.all_result = np.append(self.all_result,
-                                        np.array([self.gr_rate * self.test_popul * (1 - self.test_popul)]))
-            self.test_popul = self.all_result[-1]
+                                        np.array([self.growth_rate * self.test_initial * (1 - self.test_initial)]))
+            self.test_initial = self.all_result[-1]
             times -= 1
             continue
 
-        self.test_popul = self.initial
+        self.test_initial = self.initial
 
         if finding == 'plots':
             return self.all_result
@@ -38,17 +38,17 @@ class GraphIt:
     
     def graph(self, plot_t, per):
         round_up = len(str(per)) - 1
-        while self.gr_rate < self.limit:
+        while self.growth_rate < self.limit:
             self.all_result = np.array([])
             plt.clf()
             plt.plot(self.plot('plots', plot_t), marker='o', markersize=5, markerfacecolor='k')
             plt.plot(0, 0)
             plt.xlabel('Times')
-            plt.title('growth rate: ' + str(self.gr_rate))
+            plt.title('growth rate: ' + str(self.growth_rate))
             plt.ylabel('number of result')
             plt.draw()
             plt.pause(0.00001)
-            self.gr_rate = np.round(self.gr_rate + per, round_up)
+            self.growth_rate = np.round(self.growth_rate + per, round_up)
             continue
         return
 
@@ -57,16 +57,16 @@ class GraphIt:
         plt.plot(4, 1)
         plt.ylabel('Equilibrium')
         plt.xlabel('growth rate')
-        while self.gr_rate < 1:
-            plt.plot(self.gr_rate, 0, 'ro', markersize=0.5, markerfacecolor='k', mec='k')
+        while self.growth_rate < 1:
+            plt.plot(self.growth_rate, 0, 'ro', markersize=0.5, markerfacecolor='k', mec='k')
             plt.pause(0.000001)
-            self.gr_rate = np.round(self.gr_rate + per, round_up)
-        while self.gr_rate < self.limit:
+            self.growth_rate = np.round(self.growth_rate + per, round_up)
+        while self.growth_rate < self.limit:
             self.all_result = np.array([], dtype=float)
             y = self.plot('equilibrium', plot_t)
-            plt.plot([self.gr_rate] * (len(y)), y, 'ro', markersize=0.5, markerfacecolor='k', mec='k')
+            plt.plot([self.growth_rate] * (len(y)), y, 'ro', markersize=0.5, markerfacecolor='k', mec='k')
             plt.pause(0.000001)
-            self.gr_rate = np.round(self.gr_rate + per, round_up)
+            self.growth_rate = np.round(self.growth_rate + per, round_up)
             continue
         plt.show()
         return
@@ -76,14 +76,14 @@ class GraphIt:
         plt.plot(4, 1)
         plt.ylabel('Equilibrium')
         plt.xlabel('growth rate')
-        while self.gr_rate < 1:
-            plt.plot(self.gr_rate, 0, 'ro', markersize=0.1, markerfacecolor='k', mec='k')
-            self.gr_rate = np.round(self.gr_rate + per, round_up)
-        while self.gr_rate < self.limit:
+        while self.growth_rate < 1:
+            plt.plot(self.growth_rate, 0, 'ro', markersize=0.1, markerfacecolor='k', mec='k')
+            self.growth_rate = np.round(self.growth_rate + per, round_up)
+        while self.growth_rate < self.limit:
             self.all_result = np.array([], dtype=float)
             y = self.plot('equilibrium', plot_t)
-            plt.plot([self.gr_rate] * (len(y)), y, 'ro', markersize=0.1, markerfacecolor='k', mec='k')
-            self.gr_rate = np.round(self.gr_rate + per, round_up)
+            plt.plot([self.growth_rate] * (len(y)), y, 'ro', markersize=0.1, markerfacecolor='k', mec='k')
+            self.growth_rate = np.round(self.growth_rate + per, round_up)
             continue
         plt.show()
         return
@@ -94,26 +94,26 @@ class GraphIt:
         right.plot(self.limit, 1)
         left = plt.subplot(211)
         left.plot(0, 0)
-        
-        while self.gr_rate < 1:
+
+        while self.growth_rate < 1:
             self.all_result = np.array([], dtype=float)
-            self.gr_rate = np.round(self.gr_rate + per, round_up)
+            self.growth_rate = np.round(self.growth_rate + per, round_up)
             left.cla()
-            plt.title('growth rate: ' + str(self.gr_rate))
+            plt.title('growth rate: ' + str(self.growth_rate))
             right.set_title('population vs growth rate')
             left.plot(self.plot('plot', plot_t1), marker='o', markersize=5, markerfacecolor='k')
-            right.plot(self.gr_rate, 0, 'ro', markersize=2, markerfacecolor='k', mec='k')
+            right.plot(self.growth_rate, 0, 'ro', markersize=2, markerfacecolor='k', mec='k')
             left.plot(0, 0)
             plt.pause(0.000001)
             continue
 
-        while self.gr_rate < self.limit:
+        while self.growth_rate < self.limit:
             self.all_result = np.array([], dtype=float)
-            self.gr_rate = np.round(self.gr_rate + per, round_up)
+            self.growth_rate = np.round(self.growth_rate + per, round_up)
             left.cla()
-            plt.title('growth rate: ' + str(self.gr_rate))
+            plt.title('growth rate: ' + str(self.growth_rate))
             y = self.plot('equilibrium', plot_t2)
-            right.plot([self.gr_rate] * (len(y)), y, 'ro', markersize=2, markerfacecolor='k', mec='k')
+            right.plot([self.growth_rate] * (len(y)), y, 'ro', markersize=2, markerfacecolor='k', mec='k')
             self.all_result = np.array([], dtype=float)
             left.plot(self.plot('plots', plot_t1), marker='o', markersize=5, markerfacecolor='k')
             left.plot(0, 0)
@@ -121,3 +121,8 @@ class GraphIt:
             continue
 
         return
+
+
+graph = LogisticMap()
+
+graph.graph_2_config(100, 0.1)
